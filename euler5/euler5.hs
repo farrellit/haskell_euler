e0b17d7f723a5eedb2eb971f3fa9e8bcd3631b44
Dan Farrell <dan@farrellit.net>
2014-03-22 15:12:04 -0500

    pe 5 complete, runs in under 2 sec

--- euler5/euler5.hs
+++ euler5/euler5.hs
@@ -1,11 +1,9 @@
-import Data.List
+are_factors :: Integer -> [ Integer ] -> Bool
+are_factors num (p:ps)  
+	| null ps = ( num `mod` p == 0 )
+	| ( num `mod` p /= 0) = False
+	| otherwise = are_factors num ps 
 
-int_sqrt x = floor( sqrt( fromIntegral x ) )
+pe5 = head [ x | x <- [20,40..], are_factors x [ 20,19..2] ]
 
-largest_factors num = [ quot num x | x <- [2..(int_sqrt num ) ] , num `mod` x == 0 ]
-factors num = 1:num:[ quot num x | x <- lf ] `union` lf  -- sqrt will be duplicated if not avoided
-	where lf = ( largest_factors num )
-smallest_factor_in l = head [ x | x <- [ lmax,(2*lmax)..], sort ( factors x `intersect` l  ) == sort l ]  
-	where lmax = maximum l
--- main = putStrLn ( show ( smallest_factor_in [20,19..2] ) ) 
-
+main = putStrLn( show pe5 ) 
